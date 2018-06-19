0e30a166d3726a4b12867e353f7a8fb4a72ef6fe
Carson <cmjcards@gmail.com>
2018-06-19 18:24:51 +0000

    first_commit

---
+++ first code.py
@@ -0,0 +1,6 @@
+print ("Hello World")
+print (2+3)
+print (5**2)
+print ("What is your name?")
+name = "Carson James"
+print name [7]
---
+++ lists.py
@@ -0,0 +1,7 @@
+myshopping_list = ["bread", "milk", "cereal", "pizza",]
+for name in myshopping_list:
+    print (myshopping_list)
+    
+careers_list = ["architect", "chef", "fitness trainer",]
+for name in careers_list:
+    print (careers_list)
